12077f8206a3f8367795f5799840157925a97ca7
Andrei Chirkov <aachirkov@me.com>
2022-02-20 15:32:58 +0300

    Working two routes

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,25 @@
 import express, { Express } from 'express';
-import { userRouter } from './users/users';
 import { Server } from 'http'
 import { LoggerService } from './logger/logger.service';
+import { UsersController } from './users/users.controller';
 
 export class App {
 	app: Express  //интерфейс Express
 	server: Server
 	port: number
 	logger: LoggerService
+	userController: UsersController
 
-	constructor(logger: LoggerService) {
+	constructor(logger: LoggerService, userController: UsersController) {
 		this.app = express()  //создание инстанса express
 		this.port = 8000
 		this.logger = logger
+		this.userController = userController
 	}
 
 	useRoutes() {
-		this.app.use('/users', userRouter);
+		//app.use требует роутер, а мы его получаем через юзерКонтроллер
+		this.app.use('/users', this.userController.router);
 	}
 
 	public async init() {
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { App } from './app';
 import { LoggerService } from './logger/logger.service';
+import { UsersController } from './users/users.controller';
 
 async function bootstrap() {
 	//внедряем в app через конструктор зависимость
 	//от другого сервиса = логгерСервиса
 	//простейшая DI
-	const app = new App(new LoggerService())
+
+	const logger = new LoggerService()
+	const app = new App(logger, new UsersController(logger))
 	await app.init()
 }
 
