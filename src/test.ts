43e7bfa9b58707eb2a856bb271f3ad512b43bd7e
Andrei Chirkov <aachirkov@me.com>
2022-04-02 16:33:14 +0500

    Decorators

---
+++ src/test.ts
@@ -0,0 +1,15 @@
+function Component(target: Function) {
+	console.log(target)
+}
+
+@Component
+export class User {
+	id: number
+
+	updateId(newId: number) {
+		this.id = newId
+		return this.id
+	}
+}
+
+// console.log(new User().id)
