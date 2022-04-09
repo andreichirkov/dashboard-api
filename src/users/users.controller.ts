36c203a775abfd2007b147548fc6d17255d6c754
Andrei Chirkov <aachirkov@me.com>
2022-04-09 13:05:15 +0500

    Add dto and entity for user, hash pass

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,9 @@ import { TYPES } from '../types'
 import { ILogger } from '../logger/logger.interface'
 import 'reflect-metadata'
 import { IUserController } from './users.controller.interface'
+import { UserLoginDto } from './dto/user-login.dto'
+import { UserRegisterDto } from './dto/user-register.dto'
+import { User } from './user.entity'
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
@@ -18,12 +21,19 @@ export class UserController extends BaseController implements IUserController {
 		])
 	}
 
-	login(req: Request, res: Response, next: NextFunction): void {
-		console.log(req)
+	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
 		next(new HTTPError(401, 'Ошибка авторизации', 'login'))
 	}
 
-	register(req: Request, res: Response, next: NextFunction): void {
-		this.ok(res, 'register')
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		//Создание инстанса пользователя
+		const newUser = new User(body.email, body.name)
+		//Добавить ему захешированный пароль
+		await newUser.setPassword(body.password)
+		this.ok(res, newUser)
 	}
 }
